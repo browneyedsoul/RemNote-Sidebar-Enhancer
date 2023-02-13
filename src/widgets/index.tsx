cb3496fb040b4aba26faf4fd7654cc2249b3fdaf
Min-ki Park <browneyedsoul@yonsei.ac.kr>
2023-02-13 11:51:44 +0900

    Fix mobile rendering issue

--- src/widgets/index.tsx
+++ src/widgets/index.tsx
@@ -1,6 +1,17 @@
 import { declareIndexPlugin, ReactRNPlugin } from "@remnote/plugin-sdk";
 
-async function onActivate(plugin: ReactRNPlugin) {}
+let SidebarCSS: string;
+
+async function onActivate(plugin: ReactRNPlugin) {
+	await fetch("https://raw.githubusercontent.com/browneyedsoul/RemNote-Sidebar-Enhancer/main/src/snippet.css")
+    .then((response) => response.text())
+    .then((text) => {
+      SidebarCSS = text;
+      console.log("Sidebar Enhanced!");
+    })
+    .catch((error) => console.error(error));
+  await plugin.app.registerCSS("callout", SidebarCSS);
+}
 
 async function onDeactivate(_: ReactRNPlugin) {}
 
